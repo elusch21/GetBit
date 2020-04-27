5e6b8e041695a3245e1f6d82273ccf29f439c1d0
QijinChau <59940238+QijinChau@users.noreply.github.com>
2020-04-26 20:58:11 -0400

    test

--- NodeServer.js
+++ NodeServer.js
@@ -19,7 +19,7 @@ http.createServer(function (req, res) {
 	//parse url, decide operation
 	var qobj = url.parse(req.url, true).query;
 	var op = qobj.op;
-	var fullname = qobj.fullname, username = qobj.username, password = qobj.password;
+	var fullname = qobj.fullname, username = qobj.username, password = qobj.password, coin = qobj.coin;
 	if (op == "new") {
 		/*INSERTION INTO MONGO
 		input: username, hashed password
@@ -28,7 +28,7 @@ http.createServer(function (req, res) {
 			reason - if success false, is a string holding reason, else null
 			coins - empty array of coins for js purposes*/
 		//code here
-		var myObj = { "Full Name": fullname, "Username": username, "Password": password };
+		var myObj = { "Full Name": fullname, "Username": username, "Password": password, "Coins": };
 		MongoClient.connect(uri, function(err, db) {
 			if (err) {
 				//failure
@@ -58,7 +58,45 @@ http.createServer(function (req, res) {
 			success - string/boolean: 'true' if successful authentication, 'false' otherwise
 			reason - if success false, is a string holding reason, else null
 			coins - array of all coins that successfully authenticated user has tracked*/
-		// QIJIN - put the Mongo login checking query/code here
+		
+		var myObj = { "Username": username, "Password": password };
+		MongoClient.connect(uri, function(err, db) {
+			if (err) {
+				//failure
+				res.writeHead(301, {'Location': 'https://elusch21.github.io/GetBit/Account.html?success=false&reason=connect_fail'});
+				throw err;
+			}
+	   		// access database called "GetBit"
+	    	var dbo = db.db("GetBit");
+
+	    	// find user in the MongoDB database w/ collection called "User_Info"  
+	    	dbo.collection("User_Info").find(myObj).toArray(function(err, result) {
+		    	if (err) 
+		    		throw err;
+		    	
+		    	if(result.length == 0) {
+		    		db.close();
+		    		res.writeHead(301, {'Location': 'https://elusch21.github.io/GetBit/Account.html?success=false&reason=login_fail'});
+		    		res.end();
+		    	} else if(result[0]["Coins"] == undefined) {
+		    		db.close();
+		    		res.writeHead(301,{'Location':'https://elusch21.github.io/GetBit/Account.html?success=true&coins='});
+		    		res.end();
+		    	} else {
+		    		var string = "https://elusch21.github.io/GetBit/Account.html?success=true&coins="
+		    		for(i=0; i<result[0]["Coins"].length; i++) {
+		    			string += result[0]["Coins"][i];
+		    			if(i < result[0]["Coins"].length-1) {
+		    				string += ",";
+		    			}
+		    		}
+		    		db.close();
+		    		res.writeHead(301,{'Location': string});
+		    		res.end();
+		    	}
+		  	});
+		});
+		
 	} else if (op == "add") {
 		/*ADD COIN
 		input: username, hashed password
@@ -66,6 +104,31 @@ http.createServer(function (req, res) {
 			success - string/boolean: 'true' if successful authentication, 'false' otherwise
 			reason - if success false, is a string holding reason, else null
 			coins - array of all coins that successfully authenticated user has tracked*/
-		// QIJIN - put the Mongo add tracked coin query/code here
+		/*
+		var myObj = { "Username": username, "Password": password };
+		MongoClient.connect(uri, function(err, db) {
+			if (err) {
+				//failure
+				res.writeHead(301, {'Location': 'https://elusch21.github.io/GetBit/Account.html?success=false&reason=connect_fail'});
+				throw err;
+			}
+		   	// access database called "GetBit"
+		    var dbo = db.db("GetBit");
+
+		    // find user in the MongoDB database w/ collection called "User_Info"  
+		    dbo.collection("User_Info").find(myObj).toArray(function(err, result) {
+			   	if (err) 
+			   		throw err;
+
+			   	dbo.collection("User_Info").update(
+				   myObj,   // Query parameter
+				   { $push: { Coins: coin } }
+				)
+			   	db.close();
+			   	//res.writeHead(301,{'Location': string});
+			   	res.end();
+			});
+		});
+		*/
 	}
 }).listen(port);
